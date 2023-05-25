961e7f5e827bc0740ce3f80e3f551736b1699461
Olenakas <121051082+Olenakas@users.noreply.github.com>
2023-05-25 16:43:04 +0200

    task 2-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new VimeoPlayer(iframe);
+
+player.on('timeupdate', throttle(updateCurrentTime, 1000));
+
+function updateCurrentTime(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+if (currentTime) {
+  player.setCurrentTime(parseFloat(currentTime));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const STORAGE_KEY = 'feedback-form-state';
 
+emailInput.addEventListener('input', saveFormState);
+messageInput.addEventListener('input', saveFormState);
+
+window.addEventListener('load', loadFormState);
+
+form.addEventListener('submit', handleSubmit);
+
+function saveFormState() {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formState));
+}
+
+function loadFormState() {
+  const savedFormState = localStorage.getItem(STORAGE_KEY);
+  if (savedFormState) {
+    const formState = JSON.parse(savedFormState);
+    emailInput.value = formState.email;
+    messageInput.value = formState.message;
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log('Form submitted:', formState);
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+}
+
+const saveFormStateThrottled = require('lodash.throttle')(saveFormState, 500);
+emailInput.addEventListener('input', saveFormStateThrottled);
+messageInput.addEventListener('input', saveFormStateThrottled);
